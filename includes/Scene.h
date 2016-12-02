c168de80b134ea6723f0eed3dc7c5bd3f4eecd87
David Marchbanks <ptdave20@gmail.com>
2016-12-02 11:01:25 -0500

    Trying to setup lua (selene) with Scene and bind controls

--- includes/Scene.h
+++ includes/Scene.h
@@ -11,11 +11,76 @@
 #include "Entity.h"
 #include "Tile.h"
 
+#include <string>
+#include <selene.h>
+
+using namespace sel;
+
 class Scene {
+public:
+    Scene(std::string _name) {
+        name = name;
+    }
+
+
+    // MUSIC CONTROLS
+    void loadMusic(std::string file) {
+        music.openFromFile(file);
+    }
+
+    void startMusic() {
+        music.play();
+    }
+
+    void stopMusic() {
+        music.stop();
+    }
+
+    void setMusicVolume(const float &n) {
+        music.setVolume(n);
+    }
+
+    const float getMusicVolume() {
+        return music.getVolume();
+    }
+
+    void setMusicLoop(bool value) {
+        music.setLoop(value);
+    }
+
+    bool getMusicLoop() {
+        return music.getLoop();
+    }
+
+
+    // NAME CONTROLS
+    std::string getName() {
+        return name;
+    }
+
+    void setName(std::string _name) {
+        name = _name;
+    }
 private:
-    std::string name;
     sf::Music music;
+    std::string name;
 };
+
+void SceneLua(sel::State &state) {
+    state["Scene"].SetClass<Scene, std::string>(
+            // MUSIC CONTROLS
+            "loadMusic", &Scene::loadMusic,
+            "startMusic", &Scene::startMusic,
+            "stopMusic", &Scene::stopMusic,
+            "setMusicVolume", &Scene::setMusicVolume,
+            "getMusicVolume", &Scene::getMusicVolume,
+            "setMusicLoop", &Scene::setMusicLoop,
+            "getMusicLoop", &Scene::getMusicLoop,
+            //NAME CONTROLS
+            "getName", &Scene::getName,
+            "setName", &Scene::setName
+    );
+}
 
 
 #endif //INNER_SCENE_H
